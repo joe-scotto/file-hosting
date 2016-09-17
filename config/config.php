30e132874087a288917a6775d2d93fbab9e544c2
Joe Scotto <joe_scotto@joescotto.net>
2016-09-17 15:59:19 -0400

    Added temp fix for  issue where name was causing update when not formatted

--- config/config.php
+++ config/config.php
@@ -12,7 +12,7 @@ $GLOBALS['config'] = [
 
 // Create or update admin
 $admin = [
-    'name' => "first last",
+    'name' => ucwords(trim("first last")),
     'username' => "username",
     'password' => "password",
     'admin' => "1"
@@ -21,7 +21,7 @@ $admin = [
 // First check if user exists and data is being updated
 $user = Admin::selectUser($admin['username']);
 
-// Add new user if not found, otherwise update
+// Add new admin if not found, otherwise update
 if (!Admin::selectUser($admin['username'])) {
     // Check that username is not in use
     if (Admin::checkUsername($admin['username'])) {
@@ -29,17 +29,26 @@ if (!Admin::selectUser($admin['username'])) {
         if (Admin::createNewUser($admin['name'], $admin['username'], $admin['password'], $admin['admin'])) {
             // Attempt to create directory
             if (Admin::createUsersDirectory($admin['username'])) {
-                Utilities::setMessage('Success!', 'Admin was created successfully', 'message_modal');
+                Utilities::setMessage('Success!', 'Admin was created successfully.', 'message_modal');
             } else {
-                Utilities::setMessage('Admin Error', 'Unable to create users directory, check if it already exists or file permissions', 'message_modal');  
+                // Set Error upon failure
+                Utilities::setMessage('Admin Error', 'Unable to create users directory, check if it already exists or file permissions.', 'message_modal');  
             }
         } else {
+            // Set Error upon failure
             Utilities::setMessage('Admin Error', 'Unknown error when creating user.', 'message_modal');    
         }
     } else {
+        // Set Error upon failure
         Utilities::setMessage('Admin Error', 'Username is in use.', 'message_modal');
     }
 } else if ($admin['name'] !== $user['name'] || $admin['admin'] !== $user['admin'] || !password_verify($admin['password'], $user['password'])){
-    // Update User here
-    Admin::updateCredentials($admin['name'], $admin['username'], $admin['password'], $admin['admin']);
+    // Update admin credentials
+    if (Admin::updateCredentials($admin['name'], $admin['username'], $admin['password'], $admin['admin'])) {
+        // Output message upon success
+        Utilities::setMessage('Success!', 'Admin was updated.', 'message_modal');
+    } else {
+        // Output message if update fails
+        Utilities::setMessage('Admin Error', 'Unkown error when updating admin.', 'message_modal');
+    }
 } 
